07ec5d40eea38b41cb5336559f6e847d259defe5
Nguyễn Tuấn Anh <153104434+tuanh2401@users.noreply.github.com>
2024-12-04 09:53:45 +0700

    Update create.php

--- BTVN/30/views/products/create.php
+++ BTVN/30/views/products/create.php
@@ -8,16 +8,16 @@
 <body>
     <h1>Thêm Sản Phẩm Mới</h1>
     <form method="POST" action="../../controllers/index.php?action=create">
-        <label for="name">Tên sản phẩm:</label>
+        <label for="name">Name :</label>
         <input type="text" id="name" name="name" required>
         <br>
-        <label for="price">Giá:</label>
+        <label for="price">Price :</label>
         <input type="number" id="price" name="price" required>
         <br>
-        <label for="published_year">Năm xuất bản:</label>
+        <label for="published_year">Published Year :</label>
         <input type="number" id="published_year" name="published_year" required>
         <br>
-        <label for="information">Thông tin:</label>
+        <label for="information">Information :</label>
         <input type="text" id="information" name="information" required>
         <br>
         <button type="submit">Thêm Sản Phẩm</button>
