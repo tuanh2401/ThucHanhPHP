44fbfa5965675ed91e06b3e490075eb6e34d15fe
tuanh2401 <nguyentuananh240104@gmail.com>
2024-12-14 08:29:48 +0700

    nopbtvn

---
+++ BTVN/14/BTT04/app/Http/Controllers/ExampleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    public function show()
+    {
+        return 'Hello from the Controller';
+    }
+}
---
+++ BTVN/14/BTT04/resources/views/posts/edit.blade.php
@@ -0,0 +1,23 @@
+<div class="container h-100 mt-5">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-10 col-md-8 col-lg-6">
+            <h3>Update Post</h3>
+            <form action="{{ route('posts.update', $post->id) }}"
+                method="post">
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" name="title" value="{{ $post->title }}" required>
+                </div>
+                <div class="form-group">
+                    <label for="content">content</label>
+                    <textarea class="form-control" id="content" name="content"
+                        rows="3" required>{{ $post->content }}</textarea>
+                </div>
+                <button type="submit" class="btn mt-3 btn-primary">Update
+                    Post</button>
+            </form>
+        </div>
+    </div>
+</div>
